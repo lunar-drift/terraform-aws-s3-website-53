7b8e46cf5c44e107af00b48efa324fef8871b825
gspider8 <83229711+gspider8@users.noreply.github.com>
2024-12-13 14:08:42 -0500

    version 0.1.1 - Added CloudFront

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_s3_bucket_website_configuration" "hosting" {
 }
 
 # Create SSL Certificate
-resource "aws_acm_certificate" "cert" {
+resource "aws_acm_certificate" "main" {
   domain_name               = var.domain_name
   subject_alternative_names = ["*.${var.domain_name}"]
   validation_method         = "DNS"
@@ -36,9 +36,9 @@ resource "aws_acm_certificate" "cert" {
 }
 
 # Validate Ownership of Domain through DNS
-resource "aws_route53_record" "cert_validation" {
+resource "aws_route53_record" "certificate_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -53,9 +53,14 @@ resource "aws_route53_record" "cert_validation" {
   zone_id         = module.dns.route53_zone.zone_id
 }
 
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.certificate_validation : record.fqdn]
+}
+
 # Create a CloudFront Distribution.
 resource "aws_cloudfront_distribution" "main" {
-  depends_on          = [aws_acm_certificate.cert]
+  depends_on          = [aws_acm_certificate_validation.main]
   default_root_object = "index.html"
   is_ipv6_enabled     = true
   enabled             = true
