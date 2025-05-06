11c69ddf3f93d191865a2379868d98f819792c43
gspider8 <83229711+gspider8@users.noreply.github.com>
2025-05-06 10:13:54 -0400

    remove unused variables.

--- variables.tf
+++ variables.tf
@@ -10,17 +10,3 @@ variable "domain_name" {
   description = "Base domain name for hosted zone e.g. 'example.com'."
   type        = string
 }
-
-variable "dns_records" {
-  description = "For each record in main, include name, type, ttl, & records"
-  type        = map(any)
-  default     = { main = {} }
-}
-
-# --- DevOps ---
-
-variable "_create_r53_zone" {
-  description = "Used in testing to create r53 zones"
-  default     = false
-  type        = bool
-}
